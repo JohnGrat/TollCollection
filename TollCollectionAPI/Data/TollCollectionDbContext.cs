96320fe2033a7ddd45b5f196d560755fae98c8c4
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2024-05-03 10:04:51 +0200

    added TollCollectionDbContext

---
+++ TollCollectionAPI/Data/TollCollectionDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class TollCollectionDbContext : DbContext
+    {
+        public TollCollectionDbContext(DbContextOptions<TollCollectionDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure your model here
+        }
+    }
+}
