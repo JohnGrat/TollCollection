414113d06a529c71bb5551f687c5aff4cf1ece5b
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2024-05-03 13:15:25 +0200

    added business layer and vehiclerepository

---
+++ TollCollectionAPI/Business/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync(DateTime? startDate, DateTime? endDate);
+
+        Task<T> AddAsync(T entity);
+    }
+}
